35da6679f0d0343cc7aba128f0f609ea5b771d30
beenbiishop <ben@benbishopsb.com>
2022-11-17 02:54:34 -0500

    Finalize Layout

--- src/TestMain.java
+++ src/TestMain.java
@@ -8,8 +8,10 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -17,23 +19,24 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.KeyStroke;
+import javax.swing.border.CompoundBorder;
 import model.Image;
 
 public class TestMain extends JFrame {
 
   public TestMain() {
     super("Layout Test");
-    setLayout(new BorderLayout(15, 20));
+    setLayout(new BorderLayout(5, 5));
 
     initMenu();
     initImagePanel();
     initSidebar();
+    initMessage();
 
     setPreferredSize(new Dimension(800, 600));
     setMinimumSize(new Dimension(800, 600));
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     pack();
-
     setVisible(true);
 
 //    setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -54,32 +57,13 @@ public class TestMain extends JFrame {
     JMenuItem saveItem = new JMenuItem("Save");
     saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.META_DOWN_MASK));
     fileMenu.add(saveItem);
+    JMenuItem removeItem = new JMenuItem("Remove");
+    removeItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.META_DOWN_MASK));
+    fileMenu.add(removeItem);
     JMenuItem quitItem = new JMenuItem("Quit");
     quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.META_DOWN_MASK));
     fileMenu.add(quitItem);
     menuBar.add(fileMenu);
-
-    JMenu filterMenu = new JMenu("Filter");
-    filterMenu.add(new JMenuItem("Blur"));
-    filterMenu.add(new JMenuItem("Sharpen"));
-    filterMenu.add(new JMenuItem("Sepia"));
-    filterMenu.add(new JMenuItem("Greyscale"));
-    JMenu visualizeSubmenu = new JMenu("Visualize Component");
-    visualizeSubmenu.add(new JMenuItem("Red Channel"));
-    visualizeSubmenu.add(new JMenuItem("Green Channel"));
-    visualizeSubmenu.add(new JMenuItem("Blue Channel"));
-    visualizeSubmenu.add(new JMenuItem("Value Channel"));
-    visualizeSubmenu.add(new JMenuItem("Intensity Channel"));
-    visualizeSubmenu.add(new JMenuItem("Luma Channel"));
-    filterMenu.add(visualizeSubmenu);
-    menuBar.add(filterMenu);
-
-    JMenu transformMenu = new JMenu("Transform");
-    transformMenu.add(new JMenuItem("Brighten"));
-    transformMenu.add(new JMenuItem("Darken"));
-    transformMenu.add(new JMenuItem("Flip Horizontally"));
-    transformMenu.add(new JMenuItem("Flip Vertically"));
-    menuBar.add(transformMenu);
 
     JMenu helpMenu = new JMenu("Help");
     helpMenu.add(new JMenuItem("Loading/Saving Images"));
@@ -88,7 +72,7 @@ public class TestMain extends JFrame {
     helpMenu.add(new JMenuItem("About Image Processor"));
     menuBar.add(helpMenu);
 
-
+    add(menuBar, BorderLayout.NORTH);
   }
 
   private void initImagePanel() {
@@ -123,27 +107,38 @@ public class TestMain extends JFrame {
     imagePanel.addTab("koala", panel2);
     imagePanel.addTab("manhattan", panel3);
 
-    imagePanel.setTabComponentAt(imagePanel.indexOfTab("color-squares"), new ButtonTab(imagePanel));
-    imagePanel.setTabComponentAt(imagePanel.indexOfTab("koala"), new ButtonTab(imagePanel));
-    imagePanel.setTabComponentAt(imagePanel.indexOfTab("manhattan"), new ButtonTab(imagePanel));
-
     add(imagePanel, BorderLayout.CENTER);
 
   }
 
   private void initSidebar() {
     JPanel sidebar = new JPanel(new GridLayout(2, 0));
-    JPanel transformations = new JPanel();
-
-    transformations.setPreferredSize(new Dimension(200, 0));
+    JPanel transformations = new JPanel(new BorderLayout());
+    transformations.setBorder(BorderFactory.createTitledBorder("Transformations"));
+    JList<String> list = new JList<>(
+        new String[]{"Brighten", "Darken", "Flip Horizontally", "Flip Vertically"});
+    list.setPreferredSize(new Dimension(200, 100));
+    transformations.add(new JScrollPane(list), BorderLayout.CENTER);
+    JButton applyButton = new JButton("Apply Transformation");
+    transformations.add(applyButton, BorderLayout.SOUTH);
 
     JPanel histogram = new JPanel();
-    histogram.setBackground(Color.CYAN);
-    histogram.setPreferredSize(new Dimension(200, 0));
+    histogram.setBorder(BorderFactory.createTitledBorder("Histogram"));
 
     sidebar.add(transformations);
     sidebar.add(histogram);
-    sidebar.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 10));
+    sidebar.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 10));
     add(sidebar, BorderLayout.EAST);
+  }
+
+  private void initMessage() {
+    JPanel message = new JPanel(new BorderLayout());
+    JLabel label = new JLabel("Message would go right here");
+    label.setHorizontalAlignment(JLabel.LEFT);
+    label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    message.add(label, BorderLayout.CENTER);
+    message.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10),
+        BorderFactory.createTitledBorder("Message")));
+    add(message, BorderLayout.SOUTH);
   }
 }
