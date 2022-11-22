f71b806d2747c37a1a471c5986e3e65ec543b8f3
sm1se <rosemary.s@northeastern.edu>
2022-11-22 00:27:42 -0500

    messagepanel

--- src/view/panels/MessagePanel.java
+++ src/view/panels/MessagePanel.java
@@ -1,7 +1,19 @@
 package view.panels;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class MessagePanel extends JPanel {
+  static JFrame frame;
+  static JButton button1;
+  static JLabel label;
+
+  public static void main(String[] args) {
+    frame = new JFrame("Message Panel");
+    label = new JLabel("Message Label");
+
+    frame.setSize(100, 300);
+    frame.show();
+
+  }
 
 }
