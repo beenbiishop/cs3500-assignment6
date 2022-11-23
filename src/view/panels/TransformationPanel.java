ca4a5abc94bf67c824c96f4eac32029aa409b980
beenbiishop <ben@benbishopsb.com>
2022-11-23 17:58:38 -0500

    what

--- src/view/panels/TransformationPanel.java
+++ src/view/panels/TransformationPanel.java
@@ -2,12 +2,13 @@ package view.panels;
 
 import javax.swing.JButton;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import view.ImageProcessorGui;
 
 /**
  * Represents the transformation panel that is a part of the {@link ImageProcessorGui} view.
  */
-public class TransformationPanel extends JList<String> {
+public class TransformationPanel extends JPanel {
 
   JButton transformButton;
   JList<String> list;
@@ -19,13 +20,5 @@ public class TransformationPanel extends JList<String> {
     super();
   }
 
-  public void addTransformation(String key, String label) {
-    if (key == null || key.length() == 0) {
-      throw new IllegalArgumentException("Key cannot be null or empty");
-    }
-    if (label == null || label.length() == 0) {
-      throw new IllegalArgumentException("Label cannot be null or empty");
-    }
 
-  }
 }
